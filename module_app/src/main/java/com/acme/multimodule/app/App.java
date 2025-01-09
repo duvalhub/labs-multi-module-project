1a7c6fd41d5eb8c42a446e1fca8d73f5e3524df2
Philippe Duval <philippeduval@me.com>
2025-01-09 12:48:42 -0500

    Seulement des modules.

--- module_app/src/main/java/com/acme/multimodule/app/App.java
+++ module_app/src/main/java/com/acme/multimodule/app/App.java
@@ -1,32 +1,32 @@
-package com.acme.multimodule.app;
-
-import com.acme.multimodule.impl1.Impl_un;
-import com.acme.multimodule.spi.API;
-
-import java.util.ServiceLoader;
-
-public class App {
-
-    public static void main(String[] args) {
-
-        ServiceLoader<API> loader = ServiceLoader.load(API.class);
-
-        // Utilisation de ServiceLoader pour charger et utiliser
-        // les implémentations.
-        for (API api : loader) {
-            api.doSomething();
-        }
-
-    }
-
-    public static void main0(String[] args) {
-
-        API api1 = new Impl_un();
-        API api2 = new com.acme.multimodule.impl2.Impl_deux();
-
-        api1.doSomething();
-        api2.doSomething();
-
-    }
-
-}
+//package com.acme.multimodule.app;
+//
+//import com.acme.multimodule.impl1.Impl_un;
+//import com.acme.multimodule.spi.API;
+//
+//import java.util.ServiceLoader;
+//
+//public class App {
+//
+//    public static void main(String[] args) {
+//
+//        ServiceLoader<API> loader = ServiceLoader.load(API.class);
+//
+//        // Utilisation de ServiceLoader pour charger et utiliser
+//        // les implémentations.
+//        for (API api : loader) {
+//            api.doSomething();
+//        }
+//
+//    }
+//
+//    public static void main0(String[] args) {
+//
+//        API api1 = new Impl_un();
+//        API api2 = new com.acme.multimodule.impl2.Impl_deux();
+//
+//        api1.doSomething();
+//        api2.doSomething();
+//
+//    }
+//
+//}
--- module_app/src/main/java/module-info.java
+++ module_app/src/main/java/module-info.java
@@ -5,7 +5,12 @@ module com.acme.multimodule.module_app {
     requires com.acme.multimodule.module_impl_deux;
 
     uses com.acme.multimodule.spi.API;
+
+    // Parce qu'il y a un import dans l'implantation. Sinon, l'interface
+    //  était suffisante.
+    // TODO vérifier si je peux « overrider » modile-info.java et je
+    //  paris que la réponse est non parce que ça fait plus parti du code
+    //  que de l'environnement.
     uses com.acme.multimodule.impl1.Impl_un;
-    uses com.acme.multimodule.impl2.Impl_deux;
 
 }
