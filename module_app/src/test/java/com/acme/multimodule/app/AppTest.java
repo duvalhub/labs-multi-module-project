984a02edcc6306ff7668e48b9fc16b852b6d043f
Philippe Duval <philippeduval@me.com>
2025-01-09 12:49:00 -0500

    Seulement des modules.

---
+++ module_app/src/test/java/com/acme/multimodule/app/AppTest.java
@@ -0,0 +1,35 @@
+package com.acme.multimodule.app;
+
+import com.acme.multimodule.impl1.Impl_un;
+import com.acme.multimodule.spi.API;
+import org.junit.jupiter.api.Test;
+
+import java.util.ServiceLoader;
+
+public class AppTest {
+
+    @Test
+    public void test_1() {
+
+        ServiceLoader<API> loader = ServiceLoader.load(API.class);
+
+        // Utilisation de ServiceLoader pour charger et utiliser
+        // les implémentations.
+        for (API api : loader) {
+            api.doSomething();
+        }
+
+    }
+
+    @Test
+    public void test_2() {
+
+        API api1 = new Impl_un();
+        API api2 = new com.acme.multimodule.impl2.Impl_deux();
+
+        api1.doSomething();
+        api2.doSomething();
+
+    }
+
+}
